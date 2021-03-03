f6dd412c29ea8aac0279eaa0b35d6f90652cd438
aursedy <72765207+aursedy@users.noreply.github.com>
2021-03-03 21:26:28 +0100

    Delete School management directory

--- School management/adminPage/header.php
+++
@@ -1,4 +0,0 @@
-<header>
-	<div class="left-area">Management <span>System</span></div>
-    <div class="right-area"><a class="logout-btn" href="../logout.php">Logout</a></div>
-</header>
--- School management/studentPage/header.php
+++
@@ -1,4 +0,0 @@
-<header>
-	<div class="left-area">Student <span>page</span></div>
-    <div class="right-area"><a class="logout-btn" href="../logout.php">Logout</a></div>
-</header>
